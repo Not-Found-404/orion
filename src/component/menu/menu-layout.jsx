b693c09d9fb65e7e308267cad5e4847efd0e8487
BillowsTao <taobillows@163.com>
2019-04-19 02:02:44 +0800

    Refactor page layout.

--- src/component/menu/menu-layout.jsx
+++ src/component/menu/menu-layout.jsx
@@ -7,19 +7,19 @@ const { SubMenu } = Menu;
 export class MenuLayout extends React.Component {
   render() {
     return (
-      <Menu className="layout-sider__menu"
+      <Menu theme="dark"
         mode="inline"
         defaultSelectedKeys={['1']}
         defaultOpenKeys={['sub1']}
       >
         {/* 菜单项 */}
-        <SubMenu key="sub1" title={<span><Icon type="user" />用户</span>}>
+        <SubMenu key="sub1" title={<span><Icon type="user" /><span>用户</span></span>}>
           <Menu.Item key="1">
             {/* 路由链接 */}
             <Link to="/userManage/">用户管理</Link>
           </Menu.Item>
         </SubMenu>
-        <SubMenu key="sub2" title={<span><Icon type="shop" />店铺</span>}>
+        <SubMenu key="sub2" title={<span><Icon type="shop" /><span>店铺</span></span>}>
           <Menu.Item key="2">
             创建店铺
           </Menu.Item>
@@ -30,7 +30,7 @@ export class MenuLayout extends React.Component {
             <Link to="/shopManage/">店铺管理</Link>
           </Menu.Item>
         </SubMenu>
-        <SubMenu key="sub3" title={<span><Icon type="pay-circle" />交易</span>}>
+        <SubMenu key="sub3" title={<span><Icon type="pay-circle" /><span>交易</span></span>}>
           <Menu.Item key="9">订单管理</Menu.Item>
           <Menu.Item key="10">
             <Link to="/commentManage/">评价管理</Link>
--- src/view/home/home.jsx
+++ src/view/home/home.jsx
@@ -1,46 +1,71 @@
 import React from 'react';
-import { Layout } from 'antd';
+import { Layout, Icon } from 'antd';
 import { UserPaging } from '../../component/user/user.paging';
 import { MenuLayout } from '../../component/menu/menu-layout';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './home.css';
-import {CommentPaging} from "../../component/order/comment.paging";
-import {ShopPaging} from "../../component/shop/shop.paging";
-import {TagPaging} from "../../component/shop/tag.paging"; // 引入样式表
+import { CommentPaging } from "../../component/order/comment.paging";
+import { ShopPaging } from "../../component/shop/shop.paging";
+import { TagPaging } from "../../component/shop/tag.paging"; // 引入样式表
 
 // 界面控件
 const {
-    Header, Content, Sider,
+  Header, Content, Sider,
 } = Layout;
 
 export class Home extends React.Component {
-    render() {
-        return (
-          <Layout className="layout">
-            <Router>
-              {/* 页面头 */}
-              <Header className="layout-header" style={{ backgroundColor: "#007bff", }}>
-                <div className="logo" >O2O 管理后台</div>
-              </Header>
-              {/* 页面主体 */}
-              <Content className="layout-main">
-                <Layout style={{ padding: '24px 0', background: '#fff' }}>
-                  {/* 侧边栏 */}
-                  <Sider className="layout-sider">
-                    <MenuLayout />
-                  </Sider>
-                  {/* 内容呈现区域 */}
-                  <Content className="content-container">
-                    {/* 在此切换组件 */}
-                    <Route path="/userManage" component={UserPaging} />
-                    <Route path="/commentManage" component={CommentPaging} />
-                    <Route path="/shopManage" component={ShopPaging} />
-                    <Route path="/tagManage" component={TagPaging} />
-                  </Content>
-                </Layout>
-              </Content>
-            </Router>
+  state = {
+    collapsed: false,
+  };
+
+  /* 展开侧边栏函数 */
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  }
+
+  render() {
+    return (
+
+      <Router>
+        <Layout className="layout">
+          <Sider
+            className="layout-sider"
+            trigger={null}
+            collapsible
+            collapsed={this.state.collapsed}
+          >
+            <div className="sider-content">
+              <div className="logo">O2O</div>
+              <div className="layout-sider__menu">
+                {/* 菜单项 */}
+                <MenuLayout />
+              </div>
+            </div>
+
+          </Sider>
+          <Layout>
+            <Header style={{ background: '#fff', padding: 0 }}>
+              <Icon
+                className="trigger"
+                type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+                onClick={this.toggle}
+              />
+            </Header>
+            <Content style={{
+              margin: '24px 16px', padding: 24, background: '#fff',
+            }}
+            >
+              {/* 路由切换组件区域 */}
+              <Route path="/userManage" exact component={UserPaging} />
+              <Route path="/tagManage" exact component={TagPaging} />
+              <Route path="/shopManage" exact component={ShopPaging} />
+              <Route path="/commentManage" exact component={CommentPaging} />
+          </Content>
           </Layout>
-        );
-    }
+        </Layout>
+      </Router>
+    );
+  }
 }
